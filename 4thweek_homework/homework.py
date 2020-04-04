d6deda97605afdeea541b5996bb993fdf50fe2e9
claire-euni <61927257+claire-euni@users.noreply.github.com>
2020-04-04 22:34:40 +0900

    4th week homework-flask

---
+++ 4thweek_homework/homework.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, jsonify, request
+homework = Flask(__name__)
+
+from pymongo import MongoClient
+client = MongoClient('localhost', 27017)
+db = client.dbsparta
+
+@homework.route('/')
+def home():
+    return render_template('index.html')
+
+@homework.route('/orders', methods=['POST'])
+def put_order():
+    name_receive = request.form['name_give']
+    count_receive = request.form['count_give']
+    add_receive = request.form['add_give']
+    phone_receive = request.form['phone_give']
+
+    doc={
+        'name': name_receive,
+        'count': count_receive,
+        'address': add_receive,
+        'phone': phone_receive
+    }
+
+    db.orders.insert(doc)
+    return jsonify({'result':'success', 'msg':'주문이 성공적으로 작성되었습니다'})
+
+@homework.route('/orders', methods=['GET'])
+def receive_order():
+    result = list(db.orders.find({},{'_id': False}))
+    return jsonify({'result':'success', 'orders': result})
+
+if __name__ =='__main__':
+    homework.run('127.0.0.1', port=5000, debug=True)
